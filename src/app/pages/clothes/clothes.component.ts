dc88c618761316d885d8878e044298f86097908b
Plamen Draganov <pdragatc@training.local>
2018-08-07 19:05:22 +0300

    adding the filter

--- src/app/pages/clothes/clothes.component.ts
+++ src/app/pages/clothes/clothes.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ClothesComponent implements OnInit {
   title = 'My First Angular App!';
   
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
