c1b61ab7c6d4813cbb15a13e3bd80a3db2e14b5b
Martin Dobrudzhanski <martin.dobr@abv.bg>
2018-08-12 21:46:09 +0300

    styles-refactoring (#51)

--- src/app/services/tech.service.ts
+++ src/app/services/tech.service.ts
@@ -6,25 +6,25 @@ export class TechService{
     items = [
         {
           id: 1,
-          title: 'Cameras',
+          title: 'CAMERAS',
           text: 'Technomarket -30% Technopolis -20% Media Markt -10% InterTecno -10% Electronica -10%',
           photo: '../../assets/tech/camera.png'
         },
         {
           id: 2,
-          title: 'Mobile',
+          title: 'MOBILE',
           text: 'Electronica -15% InterTecno -20% Media Markt -5% Technopolis -10% Technomarket -20%',
           photo: '../../assets/tech/mobile.png'
         },
         {
           id: 3,
-          title: 'Laptops',
+          title: 'LAPTOPS',
           text: 'Technomarket -30% Technopolis -20% Media Markt -10% InterTecno -10% Electronica -10%',
           photo: '../../assets/tech/laptops.png'
         },
         {
           id: 4,
-          title: 'Audio',
+          title: 'AUDIO',
           text: 'Electronica -15%\n InterTecno -20%\n Media Markt -5%\n Technopolis -10%\n Technomarket -20%',
           photo: '../../assets/tech/audio.png'
         },
