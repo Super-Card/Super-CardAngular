167ea07759cb5d127b917576b9435b11e16a5a04
Plamen Draganov <pdragatc@training.local>
2018-08-13 19:38:03 +0300

    filterAdded

--- src/app/services/clothes.service.ts
+++ src/app/services/clothes.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
 
 @Injectable()
 export class ClothesService {
+	constructor() { }
+	
+	/* getResultItems() {
+    let result = this.resultItems;
+
+    if (this.filters.discounts.length > 0 && result.length > 0) {
+      result = result.filter(item => this.filters.discounts.some(p => p === item.shopName));
+    }
+	
+	if (this.filters.shopNames.length > 0 && result.length > 0) {
+      result = result.filter(item => this.filters.shopNames.some(p => p === item.shopName));
+    }
+	
+    return result;
+  } */
+  
+  /* 
+  selectedItem = {};
+
+  setSelectedItem(resultItem) {
+    this.selectedItem = resultItem;
+  }
+
+	
+	
+	
   filters: string[] = [];
  
   add(filter: string) {
@@ -12,4 +39,57 @@ export class ClothesService {
   clear() {
     this.filters = [];
   }
+  
+  filters = { discounts: [], shopNames: [] };
+  
+  setFilter(type: string, filter: string, isChecked: boolean) {
+    if (isChecked) {
+      this.filters[type].push(filter);
+    } else {
+      let array = this.filters[type];
+      array.splice(array.indexOf(filter), 1)
+    }
+  } */
+  
+  
+  resultItems = [
+    {
+      id: 1,
+      shopImage: '../../assets/clothes/serdika.jpg',
+      shopName: 'Serdika',
+      discount: '10%'
+    },
+    {
+      id: 1,
+      shopImage: '../../assets/clothes/paradise.jpg',
+      shopName: 'Paradise',
+      discount: '15%'
+    },
+    {
+      id: 1,
+      shopImage: '../../assets/clothes/plovdiv.jpg',
+      shopName: 'Plovdid',
+      discount: '20%'
+    },
+    {
+      id: 1,
+      shopImage: '../../assets/clothes/varna.jpg',
+      shopName: 'Varna',
+      discount: '25%'
+    },
+    {
+      id: 1,
+      shopImage: '../../assets/clothes/burgas.jpg',
+      shopName: 'Burgas',
+      discount: '20%'
+    },
+    {
+      id: 1,
+      shopImage: '../../assets/clothes/zagora.jpg',
+      shopName: 'Stara Zagora',
+      discount: '30%'
+    }
+ 
+  ];
+
 }
